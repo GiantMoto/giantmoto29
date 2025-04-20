453a4ec7073b52854ba63fbc19fb7695c4f57c90
GiantMoto <neuralai.noreply@gmail.com>
2025-04-20 23:07:24 +0200

    Update and rename mapa-strony.html to mapa-strony.tsx

---
+++ mapa-strony.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const MapaStrony = () => {
+  return (
+    <div>
+      <h1>Mapa strony</h1>
+      <ul>
+        <li><a href="/portfolio">Portfolio</a></li>
+        <li><a href="/uslugi">Usługi</a></li>
+        <li><a href="/kontakt">Kontakt</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/technologia">Technologia</a></li>
+        <li><a href="/o-nas">O nas</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default MapaStrony;
+
+    © 2025 Giant Motorsport. Wszystkie prawa zastrzeżone.
+  </footer>
+</body>
+</html>
